029cb032dd37c932116c7b96f2c422a56535e067
UnrealCodex <r.alan.rdz@unrealcodex.com>
2018-10-05 16:18:15 -0500

    sfdsaddf

---
+++ forma.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+
+<body>
+	
+	Nombre:
+	Apellido Paterno:
+	Apellido Materno:
+	Direccion:
+	Colonia:
+	Codigo Postal:
+	Telefono:
+	Celular:
+	Email:
+	------
+	Escuela:
+	Region:
+	
+	
+	
+	
+</body>
+</html>
